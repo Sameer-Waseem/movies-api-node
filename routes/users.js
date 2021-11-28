bd5c3a2e819f1505919701a1fc7e46394ef31f47
Sameer M. Waseem <sameerwaseem969@gmail.com>
2021-11-28 21:08:47 +0500

    Added header to access it from client

--- routes/users.js
+++ routes/users.js
@@ -47,6 +47,7 @@ router.post("/", async (req, res) => {
     const token = user.generateAuthToken();
     res
       .header("x-auth-token", token)
+      .header('access-control-expose-headers','x-auth-token')
       .status(200)
       .send({ _id: user._id, name, email });
   } catch (error) {
